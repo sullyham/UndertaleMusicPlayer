bfabec281bd1712703372bd9bb4d980602d0ff38
sullyham <himynameissullyham@gmail.com>
2025-05-31 16:13:35 -0400

    Copy of my IntelliJ Project

---
+++ src/main/java/org/sally/MyFrame.java
@@ -0,0 +1,28 @@
+package org.sally;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    public MyFrame() {
+        ImageIcon image = new ImageIcon("gameimage.png");
+
+       MyLabel label = new MyLabel();
+
+
+        this.add(label);
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setIconImage(image.getImage());
+        this.getContentPane().setBackground(new Color(0, 0, 0));
+        this.setResizable(true);
+        this.setTitle("GUI");
+        this.setLayout(null);
+        this.setVisible(true);
+        this.setSize(1220   , 1220);
+
+
+
+
+    }
+}
---
+++ src/main/java/org/sally/MyLabel.java
@@ -0,0 +1,25 @@
+package org.sally;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class MyLabel extends JLabel {
+    public MyLabel(){
+        Border border = BorderFactory.createLineBorder(Color.red, 3);
+        ImageIcon image2 = new ImageIcon("cursed.jpeg");
+        this.setText("Are you lost?");
+        this.setHorizontalTextPosition(this.CENTER);
+        this.setVerticalTextPosition(this.TOP);
+        this.setIconTextGap(-75);
+        this.setForeground(Color.red);
+        this.setFont(new Font("Another Danger - Demo", Font.PLAIN,60));
+        this.setIcon(image2);
+        this.setBackground(Color.white);
+        this.setOpaque(true);
+        this.setVerticalAlignment(JLabel.CENTER);
+        this.setHorizontalAlignment(JLabel.CENTER);
+        this.setBounds(100,100,800,800);
+    }
+
+}
