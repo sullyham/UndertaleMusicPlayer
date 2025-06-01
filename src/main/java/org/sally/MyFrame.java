a0821c8d975eaf13219190bd271843650a95a2eb
sullyham <himynameissullyham@gmail.com>
2025-06-01 18:44:04 -0400

    sync changes so i can access on linux laptop

--- src/main/java/org/sally/MyFrame.java
+++ src/main/java/org/sally/MyFrame.java
@@ -8,18 +8,14 @@ import java.awt.desktop.SystemEventListener;
 public class MyFrame extends JFrame {
     public MyFrame() {
         ImageIcon image = new ImageIcon("gameimage.png");
-
-
-
-
-        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-        this.setIconImage(image.getImage());
-        this.getContentPane().setBackground(new Color(255, 255, 255));
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE); //exits program on close
+        this.setIconImage(image.getImage()); //sets program icon
+        this.getContentPane().setBackground(new Color(255, 255, 255)); //background of frame
         this.setResizable(true);
         this.setTitle("GUI");
-       this.setLayout(null);
+       this.setLayout(null); //choose layout mngrs
         this.setVisible(true);
-        this.setSize(1280   , 720);
+        this.setSize(1280   , 720); //resolution
        // this.pack(); //automatically sets window size to accommodate elements
 
 
